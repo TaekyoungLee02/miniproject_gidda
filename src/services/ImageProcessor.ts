731a27ac0d761da2c08d3ce29d76f5a7be182d22
jiyeon back <spacedawn@knou.ac.kr>
2026-01-22 15:54:08 +0900

    feat: 이미지 전처리 서비스 추가

---
+++ src/services/ImageProcessor.ts
@@ -0,0 +1,26 @@
+import * as ImageManipulator from 'expo-image-manipulator';
+import { Service } from "@/src/lib/interfaces/service";
+
+export class ImageProcessorService implements Service {
+  
+  /**
+   * @param uri 원본 이미지 경로
+   */
+  async processForMobileClip(uri: string) {
+    try {
+      const result = await ImageManipulator.manipulateAsync(
+        uri,
+        [{ resize: { width: 256, height: 256 } }], // 1. 사이즈 조절 256x256
+        { 
+          compress: 1, 
+          format: ImageManipulator.SaveFormat.JPEG // 2. RGB 형식을 위해 JPEG 선택 (RGBA 제거)
+        }
+      );
+      
+      return result.uri; // 전처리된 이미지의 새로운 경로 반환
+    } catch (error) {
+      console.error("이미지 전처리 중 에러 발생:", error);
+      throw error;
+    }
+  }
+}
