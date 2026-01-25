50e7636829a1586dfdf8a987dc97a1a94174355e
TaekyoungLee02 <102038651+TaekyoungLee02@users.noreply.github.com>
2026-01-25 16:55:06 +0900

    [ADD] Model Singleton Added

--- src/model/Model.ts
+++ src/model/Model.ts
@@ -7,8 +7,17 @@ import * as Session from '@/src/model/session/ModelInferenceSession'
  */
 export class ImageEncoder extends Session.ModelInferenceSession
 {
-    constructor() {
+    // Singleton
+    private static instance : ImageEncoder;
+
+    private constructor() {
         super(ModelType.Image, [1, 3, 256, 256], "float32");
+    }
+
+    // get instance
+    public static getInstance(): ImageEncoder
+    {
+        return this.instance || (this.instance = new this())
     }
 }
 
@@ -18,6 +27,9 @@ export class ImageEncoder extends Session.ModelInferenceSession
  */
 export class TextEncoder extends Session.ModelInferenceSession
 {
+    // Singleton
+    private static instance : TextEncoder;
+
     // text tokenizer session
     tokenizer : TextTokenizerSession;
 
@@ -26,19 +38,28 @@ export class TextEncoder extends Session.ModelInferenceSession
         this.tokenizer = new TextTokenizerSession();
     }
 
+    // get instance
+    public static getInstance(): TextEncoder
+    {
+        return this.instance || (this.instance = new this())
+    }
+
     async initialize(): Promise<void> {
         await super.initialize();
-        await this.tokenizer.initialize();
     }
 
     async run(input : string)
     {
+        if (!this.tokenizer.session) await this.tokenizer.initialize();
+
         let data = await this.tokenizer.run(input)
         return await super.run(data);
     }
 
     async runEnumerate(input : string[])
     {
+        if (!this.tokenizer.session) await this.tokenizer.initialize();
+
         let datas = await this.tokenizer.runEnumerate(input);
         return super.runEnumerate(datas);
     }
